e53043d2b3b56a18f7b1aa9431c9e492b8373be6
notiuneoffiicial <notiuneofficial@gmail.com>
2025-09-21 16:03:42 +0500

    Update OptimismTextarea.tsx

--- client/src/components/Chat/Input/OptimismTextarea.tsx
+++ client/src/components/Chat/Input/OptimismTextarea.tsx
@@ -1,12 +1,10 @@
 import React, { forwardRef } from 'react';
 import RTA from 'react-textarea-autosize';
-import { TextareaAutosize as BaseTA } from '@librechat/client';
 
 type Props = React.ComponentProps<typeof RTA>;
 
 const OptimismTextarea = forwardRef<HTMLTextAreaElement, Props>(
   ({ placeholder: _ignored, ...rest }, ref) => (
-    <BaseTa
     <RTA
       ref={ref}
       {...rest}
