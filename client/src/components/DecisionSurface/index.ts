afdd0b43756e234898f788458decf4e1ba72098a
Arbad Tahir <arbadtahir@gmnail.com>
2026-01-21 01:15:08 +0500

    build error fix

--- client/src/components/DecisionSurface/index.ts
+++ client/src/components/DecisionSurface/index.ts
@@ -6,7 +6,6 @@
 export { default as ThinkingField } from './ThinkingField';
 export { default as DecisionComposer } from './DecisionComposer';
 export { default as ThoughtNode } from './ThoughtNode';
-export { default as SatelliteNode } from './SatelliteNode';
 export { default as LeaningIndicator } from './LeaningIndicator';
 export { default as TraceOverlay } from './TraceOverlay';
 export { default as MergeAnimation } from './MergeAnimation';
